5c5eefb8a45613fd4be94c5e108d6f62b64cf358
MarcoPaganoni <paganma@usi.ch>
2022-02-01 16:00:13 +0100

    Main project skeleton

---
+++ jayproof-core/src/integration/java/jayproof/MainIntegrationTest.java
@@ -0,0 +1,14 @@
+package jayproof;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class MainIntegrationTest {
+
+    @Test
+    public void testTest() {
+        assertTrue(true);
+    }
+
+}
---
+++ jayproof-core/src/test/java/jayproof/MainTest.java
@@ -0,0 +1,14 @@
+package jayproof;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class MainTest {
+
+    @Test
+    public void testTest() {
+        assertTrue(true);
+    }
+
+}

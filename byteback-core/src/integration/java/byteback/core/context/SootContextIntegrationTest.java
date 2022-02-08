2bb9bd6861d2be9fea02451c531a93244bb64462
MarcoPaganoni <paganma@usi.ch>
2022-02-08 13:38:37 +0100

    Testing loading a single class in the SootContext.

--- byteback-core/src/integration/java/byteback/core/context/SootContextIntegrationTest.java
+++ byteback-core/src/integration/java/byteback/core/context/SootContextIntegrationTest.java
@@ -1,13 +1,33 @@
 package byteback.core.context;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.FileNotFoundException;
+import java.nio.file.Path;
+
+import org.junit.Test;
+
+import byteback.core.ResourcesUtil;
 
 public class SootContextIntegrationTest {
 
+    @Test
     public void Instance_CalledTwice_ReturnsSameContext() {
-        SootContext a = SootContext.instance();
-        SootContext b = SootContext.instance();
+        final SootContext a = SootContext.instance();
+        final SootContext b = SootContext.instance();
         assertEquals(a, b);
+    }
+
+    @Test
+    public void LoadClass_CalledOnce_IncreasesClassesCountBy1() throws FileNotFoundException {
+        final Path classPath = ResourcesUtil.getJarPath("java8");
+        final SootContext context = SootContext.instance();
+        final int oldCount = context.getClassesCount();
+        context.prependClassPath(classPath);
+        context.loadClass("byteback.dummy.java8.Unit");
+        final int newCount = context.getClassesCount();
+        assertTrue(oldCount == newCount - 1);
     }
 
 }

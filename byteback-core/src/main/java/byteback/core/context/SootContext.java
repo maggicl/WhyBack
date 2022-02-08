0ea8b244172038d0da120fc8348134c6e441c36f
MarcoPaganoni <paganma@usi.ch>
2022-02-08 12:33:49 +0100

    Added configuration for the SootContext.

--- byteback-core/src/main/java/byteback/core/context/SootContext.java
+++ byteback-core/src/main/java/byteback/core/context/SootContext.java
@@ -1,13 +1,23 @@
 package byteback.core.context;
 
+import java.nio.file.Path;
+
 import soot.Scene;
 import soot.SootClass;
+import soot.options.Options;
 
 /**
  * Instance encapsulating the state of the Soot program analysis framework.
  * <p>
- * Note that this class is intended to be a complete wrapper for every singleton
- * managed by the Soot framework.
+ * This class is intended to be a complete wrapper for every singleton managed
+ * by the Soot framework.
+ * <p>
+ * Note that since this class wraps every singleton that is managed by Soot,
+ * modifying the global soot options externally may result in an undefined
+ * behavior of this context. To solve this, one possible solution could be to
+ * isolate the Soot singletons by using another classloader. This would in
+ * principle also allow us to keep multiple Soot contexts at the same time,
+ * without being constrained to the singleton pattern.
  */
 public class SootContext implements Context {
 
@@ -31,35 +41,47 @@ public class SootContext implements Context {
     private final Scene scene;
 
     /**
-     * Initializes fields with Soot singletons.
+     * The options for the scene.
+     */
+    private final Options options;
+
+    /**
+     * Initializes fields with the singletons and sets the global Soot options.
      */
     private SootContext() {
         this.scene = Scene.v();
+        this.options = Options.v();
+        this.options.set_output_format(Options.output_format_jimple);
+        this.options.set_whole_program(true);
+        this.options.set_prepend_classpath(true);
+        this.scene.loadNecessaryClasses();
     }
 
     /**
-     * Loads a new class based on the canonical name.
+     * Prepends the classpath of the Soot scene.
      *
-     * @param canonicalName The canonical name of the class.
+     * @param path The path to be prepended to the classpath.
      */
-    public void loadClass(String canonicalName) {
+    public void prependClassPath(final Path path) {
+        options.set_soot_classpath(path.toString());
+    }
+
+    /**
+     * Loads a class in the Soot scene.
+     *
+     * @param canonicalName A name of a class present in the Soot classpath.
+     */
+    @Override
+    public void loadClass(final String canonicalName) {
         scene.loadClass(canonicalName, SootClass.BODIES);
     }
 
     /**
-     * Loads a new class based on the canonical name.
+     * Computes the number of classes loaded in the Soot scene.
      *
-     * @param canonicalName The canonical name of the class.
+     * @return The total number of classes in the Soot scene.
      */
-    public void loadClassAndSupport(String canonicalName) {
-        scene.loadClassAndSupport(canonicalName);
-    }
-
-    /**
-     * Computes the total number of loaded classes.
-     *
-     * @return Total number of classes loaded in the context.
-     */
+    @Override
     public int getClassesCount() {
         return scene.getClasses().size();
     }

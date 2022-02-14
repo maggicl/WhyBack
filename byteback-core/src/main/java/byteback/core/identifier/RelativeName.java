9a95f34842437c349c184a217b1061752f224e5c
MarcoPaganoni <paganma@usi.ch>
2022-02-14 14:02:38 +0100

    Add RelativeName class

---
+++ byteback-core/src/main/java/byteback/core/identifier/RelativeName.java
@@ -0,0 +1,46 @@
+package byteback.core.identifier;
+
+/**
+ * Represents a simple relative name, taken outside the context of any
+ * namespace.
+ */
+public class RelativeName extends QualifiedName {
+
+    /**
+     * Constructs a relative name from a string.
+     *
+     * @param name A string representing the relative name.
+     * @return The {@code SimpleName} instance.
+     * @throws IllegalArgumentException if the name is not valid.
+     */
+    public static RelativeName get(final String name) {
+        final RelativeName instance = new RelativeName(name);
+
+        if (!instance.validate()) {
+            throw new IllegalArgumentException("Invalid simple name " + name);
+        }
+
+        return instance;
+    }
+
+    /**
+     * Checks if the relative name is valid by using the {@link QualifiedName}
+     * preconditions and the absence of a namespace.
+     *
+     * @return {@code true} if the name is valid.
+     */
+    @Override
+    boolean validate() {
+        return toString().split("\\.").length == 0 && super.validate();
+    }
+
+    /**
+     * Constructs the relative name from its string representation.
+     * 
+     * @param name String representing the relative name.
+     */
+    RelativeName(String name) {
+        super(name);
+    }
+
+}

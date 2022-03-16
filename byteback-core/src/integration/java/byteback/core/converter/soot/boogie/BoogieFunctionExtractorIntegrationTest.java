b86922a0100f092a29f86456f1332cb6456ba13a
Marco Paganoni <marco.paganoni@usi.ch>
2022-03-16 11:37:42 +0100

    Add logging to regression tests

--- byteback-core/src/integration/java/byteback/core/converter/soot/boogie/BoogieFunctionExtractorIntegrationTest.java
+++ byteback-core/src/integration/java/byteback/core/converter/soot/boogie/BoogieFunctionExtractorIntegrationTest.java
@@ -3,15 +3,20 @@ package byteback.core.converter.soot.boogie;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import byteback.frontend.boogie.ast.PrintUtil;
 
 public class BoogieFunctionExtractorIntegrationTest extends BoogieFunctionExtractorFixture {
 
+    private final Logger log = LoggerFactory.getLogger(BoogieFunctionExtractorFixture.class);
+
     @Test
     public void Convert_GivenRegressedFunction_ReturnsExpectedResult() {
         getFunctionEntries().forEach((entry) -> {
             assertEquals(PrintUtil.toString(entry.expected), PrintUtil.toString(entry.actual));
+            log.info("Comparing the generated function declaration {}", entry.expected.getDeclarator().getName());
         });
     }
 

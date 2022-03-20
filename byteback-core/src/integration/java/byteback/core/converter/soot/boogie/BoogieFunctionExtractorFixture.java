34cff7be9176da4cb1db807c2623fa5f8e9eaa05
Marco Paganoni <marco.paganoni@usi.ch>
2022-03-20 22:00:32 +0100

    Clean tests

--- byteback-core/src/integration/java/byteback/core/converter/soot/boogie/BoogieFunctionExtractorFixture.java
+++ byteback-core/src/integration/java/byteback/core/converter/soot/boogie/BoogieFunctionExtractorFixture.java
@@ -1,12 +1,16 @@
 package byteback.core.converter.soot.boogie;
 
-import java.nio.file.Path;
+import java.io.IOException;
+import java.util.AbstractMap.SimpleEntry;
+import java.util.Map.Entry;
 import java.util.stream.Stream;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import beaver.Parser;
 import byteback.core.ResourcesUtil;
+import byteback.core.representation.soot.unit.SootClassUnit;
 import byteback.core.representation.soot.unit.SootMethodUnitFixture;
 import byteback.frontend.boogie.ast.Program;
 import byteback.frontend.boogie.util.ParserUtil;
@@ -15,39 +19,23 @@ public class BoogieFunctionExtractorFixture extends SootMethodUnitFixture {
 
     private static final Logger log = LoggerFactory.getLogger(BoogieFunctionExtractorFixture.class);
 
-    public static class MethodIdentifier {
+    public static Stream<Entry<SootClassUnit, Program>> getRegressionEntries(final String jarName) throws IOException {
+        return ResourcesUtil.getBoogiePaths(jarName).flatMap((path) -> {
+            final String fileName = path.getFileName().toString();
+            final String className = fileName.substring(0, fileName.lastIndexOf("."));
+            final SootClassUnit classUnit = getClassUnit(jarName, className);
 
-        public final String className;
+            try {
+                final Program program = ParserUtil.parseBoogieProgram(path);
+                return Stream.of(new SimpleEntry<>(classUnit, program));
+            } catch (final IOException exception) {
+                log.error("Error while opening Boogie file {}", path, exception);
+            } catch (final Parser.Exception exception) {
+                log.error("Error while parsing Boogie file {}", path, exception);
+            }
 
-        public final String methodName;
-
-        public MethodIdentifier(final String className, final String methodName) {
-            this.className = className;
-            this.methodName = methodName;
-        }
-
-    }
-
-    public static Stream<Program> getExpectedBoogiePrograms(final String jarName) {
-        try {
-            final Stream<Path> paths = ResourcesUtil.getBoogiePaths(jarName);
-
-            return paths.flatMap((path) -> {
-                try {
-                    return Stream.of(ParserUtil.parseBoogieProgram(path));
-                } catch (final Exception exception) {
-                    log.error("Could not parse the program at {}", path, exception);
-
-                    return Stream.empty();
-                }
-            });
-        } catch (final Exception exception) {
-            throw new RuntimeException(exception);
-        }
-    }
-
-    public static Program getExpectedBoogieProgram(final String jarName) {
-        return getExpectedBoogiePrograms(jarName).reduce(new Program(), (a, b) -> (a.merge(b)));
+            return Stream.empty();
+        });
     }
 
 }
--- byteback-core/src/integration/java/byteback/core/converter/soot/boogie/BoogieFunctionExtractorIntegrationTest.java
+++ byteback-core/src/integration/java/byteback/core/converter/soot/boogie/BoogieFunctionExtractorIntegrationTest.java
@@ -2,23 +2,16 @@ package byteback.core.converter.soot.boogie;
 
 import static org.junit.Assert.assertEquals;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.Optional;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import byteback.core.RegressionParameter;
-import byteback.core.ResourcesUtil;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import byteback.core.representation.soot.annotation.SootAnnotation;
 import byteback.core.representation.soot.unit.SootClassUnit;
 import byteback.frontend.boogie.ast.FunctionDeclaration;
 import byteback.frontend.boogie.ast.PrintUtil;
@@ -27,53 +20,35 @@ import byteback.frontend.boogie.ast.Program;
 @RunWith(Parameterized.class)
 public class BoogieFunctionExtractorIntegrationTest extends BoogieFunctionExtractorFixture {
 
-    private static final Logger log = LoggerFactory.getLogger(BoogieFunctionExtractorIntegrationTest.class);
-
     @Parameters
-    public static Iterable<RegressionParameter<FunctionDeclaration>> getFunctionEntries() throws IOException {
-        final String targetJar = "java8";
-        final Program program = getExpectedBoogieProgram(targetJar);
-
-        return ResourcesUtil.getBoogiePaths(targetJar).flatMap((path) -> {
-            final String fileName = path.getFileName().toString();
-            final String className = fileName.substring(0, fileName.lastIndexOf("."));
-            final SootClassUnit classUnit = getClassUnit(targetJar, className);
+    public static Iterable<RegressionParameter<FunctionDeclaration>> getParameters() throws IOException {
+        return getRegressionEntries("java8").flatMap((entry) -> {
+            final SootClassUnit classUnit = entry.getKey();
+            final Program program = entry.getValue();
 
             return classUnit.methods().flatMap((methodUnit) -> {
-                try {
-                    final Optional<SootAnnotation> pureAnnotation = methodUnit
-                            .getAnnotation("Lbyteback/annotations/Contract$Pure;");
+                if (methodUnit.getAnnotation("Lbyteback/annotations/Contract$Pure;").isPresent()) {
+                    final String boogieName = BoogieNameConverter.methodName(methodUnit);
+                    final FunctionDeclaration expected = program.lookupFunction(boogieName).get()
+                            .getFunctionDeclaration();
+                    final FunctionDeclaration actual = new BoogieFunctionExtractor(methodUnit).convert();
 
-                    System.out.println(classUnit.getName());
-
-                    if (pureAnnotation.isPresent()) {
-                        final String boogieName = BoogieNameConverter.methodName(methodUnit);
-                        final FunctionDeclaration expected = program.lookupFunction(boogieName)
-                                .orElseThrow(() -> new IllegalAccessError(
-                                        "Could not find Boogie definition for method " + methodUnit.getName()))
-                                .getFunctionDeclaration();
-                        final FunctionDeclaration actual = new BoogieFunctionExtractor(methodUnit).convert();
-
-                        return Stream.of(new RegressionParameter<>(expected, actual));
-                    }
-                } catch (final IllegalAccessError exception) {
-                    log.error("Skipping method {} of class {}", methodUnit.getName(), classUnit.getName(), exception);
+                    return Stream.of(new RegressionParameter<>(expected, actual));
                 }
-
                 return Stream.empty();
             });
         })::iterator;
     }
 
-    private final RegressionParameter<FunctionDeclaration> entry;
+    private final RegressionParameter<FunctionDeclaration> parameter;
 
-    public BoogieFunctionExtractorIntegrationTest(final RegressionParameter<FunctionDeclaration> entry) {
-        this.entry = entry;
+    public BoogieFunctionExtractorIntegrationTest(final RegressionParameter<FunctionDeclaration> parameter) {
+        this.parameter = parameter;
     }
 
     @Test
     public void test() {
-        assertEquals(PrintUtil.toString(entry.actual), PrintUtil.toString(entry.expected));
+        assertEquals(PrintUtil.toString(parameter.actual), PrintUtil.toString(parameter.expected));
     }
 
 }

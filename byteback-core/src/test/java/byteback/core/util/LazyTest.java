93147b7a3efabc3d7f96b27fc2f36e23c048b1a9
Marco Paganoni <marco.paganoni@usi.ch>
2022-03-21 15:18:44 +0100

    Added unit tests for util classes

---
+++ byteback-core/src/test/java/byteback/core/util/LazyTest.java
@@ -0,0 +1,24 @@
+package byteback.core.util;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class LazyTest {
+
+    @Test
+    public void Get_GivenInitializedValue_ReturnsSameInitializedValue() {
+        final Object value = new Object();
+        final Lazy<Object> lazyValue = Lazy.from(() -> value);
+        assertEquals(lazyValue.get(), value);
+    }
+
+    @Test
+    public void Get_CalledTwice_ReturnsSameValue() {
+        final Lazy<Object> lazyValue = Lazy.from(() -> new Object());
+        final Object first = lazyValue.get();
+        final Object second = lazyValue.get();
+        assertEquals(first, second);
+    }
+
+}

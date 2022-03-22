9e2368c59ccbbc13de69f56afd4faaa6ce8e3e0f
Marco Paganoni <marco.paganoni@usi.ch>
2022-03-22 11:23:49 +0100

    Add dummy class for static array methods

---
+++ byteback-dummy/java8/src/main/java/byteback/dummy/ArrayMethods.java
@@ -0,0 +1,16 @@
+package byteback.dummy;
+
+public class ArrayMethods {
+
+    public static int arraySum(int[] as) {
+
+        int c = 0;
+
+        for (int a : as) {
+            c += a;
+        }
+
+        return c;
+    }
+
+}

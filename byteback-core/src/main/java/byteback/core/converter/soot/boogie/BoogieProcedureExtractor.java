af9788228b249b68153481b5d50936570d875611
MarcoPaganoni <paganma@usi.ch>
2022-03-23 15:44:02 +0100

    Add parameters separately from local variables

--- byteback-core/src/main/java/byteback/core/converter/soot/boogie/BoogieProcedureExtractor.java
+++ byteback-core/src/main/java/byteback/core/converter/soot/boogie/BoogieProcedureExtractor.java
@@ -63,80 +63,90 @@ public class BoogieProcedureExtractor extends SootStatementVisitor<ProcedureDecl
 		addStatement(new ReturnStatement());
 	}
 
-	public void addLocal(final Local local) {
-		final SootType type = new SootType(local.getType());
-		final TypeAccess typeAccess = new BoogieTypeAccessExtractor().visit(type);
-		final BoundedBinding binding = new BoundedBindingBuilder().addName(local.getName()).typeAccess(typeAccess)
-				.build();
-		final VariableDeclaration variableDeclaration = new VariableDeclarationBuilder().addBinding(binding).build();
+  public BoundedBinding bind(final Local local) {
+    final SootType type = new SootType(local.getType());
+    final TypeAccess typeAccess = new BoogieTypeAccessExtractor().visit(type);
+    final BoundedBinding binding = new BoundedBindingBuilder().addName(local.getName()).typeAccess(typeAccess)
+      .build();
+    initialized.add(local);
 
-		body.addLocalDeclaration(variableDeclaration);
-		initialized.add(local);
-	}
+    return binding;
+  }
 
-	@Override
-	public void caseIdentityStmt(final IdentityStmt identity) {
-		final SootExpression left = new SootExpression(identity.getLeftOp());
-		final Local local = new SootLocalExtractor().visit(left);
-		addLocal(local);
-	}
+  public void addLocal(final Local local) {
+    final BoundedBinding binding = bind(local);
+    final VariableDeclaration variableDeclaration = new VariableDeclarationBuilder().addBinding(binding).build();
+    body.addLocalDeclaration(variableDeclaration);
+  }
 
-	@Override
-	public void caseAssignStmt(final AssignStmt assignment) {
-		final SootExpression left = new SootExpression(assignment.getLeftOp());
-		final SootExpression right = new SootExpression(assignment.getRightOp());
+  public void addParameter(final Local local) {
+    final BoundedBinding binding = bind(local);
+    signatureBuilder.addInputBinding(binding);
+  }
 
-		left.apply(new SootExpressionVisitor<>() {
+  @Override
+  public void caseIdentityStmt(final IdentityStmt identity) {
+    final SootExpression left = new SootExpression(identity.getLeftOp());
+    final Local local = new SootLocalExtractor().visit(left);
+    addParameter(local);
+  }
 
-			@Override
-			public void caseLocal(final Local local) {
-				final SootType type = new SootType(local.getType());
-				final Assignee assignee = new Assignee();
-				final Expression expression = new BoogieExpressionExtractor(type).visit(right);
-				assignee.setReference(new ValueReference(new Accessor(local.getName())));
+  @Override
+  public void caseAssignStmt(final AssignStmt assignment) {
+    final SootExpression left = new SootExpression(assignment.getLeftOp());
+    final SootExpression right = new SootExpression(assignment.getRightOp());
 
-				if (!initialized.contains(local)) {
-					addLocal(local);
-				}
+    left.apply(new SootExpressionVisitor<>() {
 
-				addSingleAssignment(assignee, expression);
-			}
+        @Override
+        public void caseLocal(final Local local) {
+          final SootType type = new SootType(local.getType());
+          final Assignee assignee = new Assignee();
+          final Expression expression = new BoogieExpressionExtractor(type).visit(right);
+          assignee.setReference(new ValueReference(new Accessor(local.getName())));
 
-			@Override
-			public void caseDefault(final Value expression) {
-				throw new IllegalArgumentException("Conversion for assignee of type " + expression.getClass().getName()
-						+ " is currently not supported");
-			}
+          if (!initialized.contains(local)) {
+            addLocal(local);
+          }
 
-		});
+          addSingleAssignment(assignee, expression);
+        }
 
-	}
+        @Override
+        public void caseDefault(final Value expression) {
+          throw new IllegalArgumentException("Conversion for assignee of type " + expression.getClass().getName()
+                                             + " is currently not supported");
+        }
 
-	@Override
-	public void caseReturnVoidStmt(final ReturnVoidStmt returns) {
-		addReturnStatement();
-	}
+      });
 
-	@Override
-	public void caseReturnStmt(final ReturnStmt returns) {
-		final SootExpression operand = new SootExpression(returns.getOp());
-		final ValueReference valueReference = BoogiePrelude.getReturnValueReference();
-		final Assignee assignee = new Assignee(valueReference);
-		final Expression expression = new BoogieExpressionExtractor(operand.getType()).visit(operand);
-		addSingleAssignment(assignee, expression);
-		addReturnStatement();
-	}
+  }
+
+  @Override
+  public void caseReturnVoidStmt(final ReturnVoidStmt returns) {
+    addReturnStatement();
+  }
+
+  @Override
+  public void caseReturnStmt(final ReturnStmt returns) {
+    final SootExpression operand = new SootExpression(returns.getOp());
+    final ValueReference valueReference = BoogiePrelude.getReturnValueReference();
+    final Assignee assignee = new Assignee(valueReference);
+    final Expression expression = new BoogieExpressionExtractor(operand.getType()).visit(operand);
+    addSingleAssignment(assignee, expression);
+    addReturnStatement();
+  }
 
   @Override
   public void caseIfStmt(final IfStmt ifStatement) {
   }
 
-	@Override
-	public void caseDefault(final Unit unit) {
-		throw new UnsupportedOperationException("Cannot collect statements of type " + unit.getClass().getName());
-	}
+  @Override
+  public void caseDefault(final Unit unit) {
+    throw new UnsupportedOperationException("Cannot collect statements of type " + unit.getClass().getName());
+  }
 
-	@Override
+  @Override
 	public ProcedureDeclaration result() {
 		return procedureBuilder.signature(signatureBuilder.build()).body(body).build();
 	}

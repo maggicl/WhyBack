aab6a28adc29ff80f49d0d3a3b8be2aa705989f1
Marco Paganoni <marco.paganoni@usi.ch>
2022-03-28 14:46:04 +0200

    Add example assigning reference field

--- byteback-dummy/java8/src/main/java/byteback/dummy/procedure/Field.java
+++ byteback-dummy/java8/src/main/java/byteback/dummy/procedure/Field.java
@@ -4,8 +4,14 @@ public class Field {
 
 	public int field;
 
+  public Object object;
+
 	public void assignsField() {
 		field = 1;
 	}
+
+  public void assignsObject() {
+    object = null;
+  }
 
 }

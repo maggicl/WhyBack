15e4d42aff82b962feff94f0f6a0fdbe4f3611fb
Marco Paganoni <marco.paganoni@usi.ch>
2023-04-05 19:15:30 +0200

    Add test with try-with-resources-finally

--- byteback-test/test-java8/src/main/java/byteback/test/exceptions/TryWithResources.java
+++ byteback-test/test-java8/src/main/java/byteback/test/exceptions/TryWithResources.java
@@ -6,12 +6,23 @@ package byteback.test.exceptions;
 import java.io.FileReader;
 import java.io.IOException;
 
+import byteback.annotations.Contract.Return;
+
 public class TryWithResources {
 
-	public void test() throws IOException {
+	public void tryWithResources() throws IOException {
 		try (FileReader ac = new FileReader("")) {
 			System.out.println();
 		}
+	}
+
+	@Return
+	public void tryWithResourcesFinally() throws IOException {
+		try (FileReader ac = new FileReader("")) {
+			System.out.println();
+		} finally {
+			System.out.println();
+		} 
 	}
 
 }

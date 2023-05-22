1a6f17abeda75ea56e7b06c01358ae5b7edaa5dd
Marco Paganoni <marco.paganoni@usi.ch>
2023-05-22 18:04:28 +0200

    Add kotlin to the formatted experiments

--- byteback-test/format-experiments.sh
+++ byteback-test/format-experiments.sh
@@ -1,4 +1,4 @@
 #!/bin/env bash
 
 echo "% DATE: $(date)"
-python format-keys.py ./test-java-8/benchmark/benchmark.csv j8 ./test-java-17/benchmark/benchmark.csv j17 ./test-scala-2/benchmark/benchmark.csv s2 --prefix /byteback/test/
+python format-keys.py ./test-java-8/benchmark/benchmark.csv j8 ./test-java-17/benchmark/benchmark.csv j17 ./test-scala-2/benchmark/benchmark.csv s2 ./test-kotlin-1.8.0/benchmark/benchmark.csv --prefix /byteback/test/
